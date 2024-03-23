c00d0e021f7a9645c0c97cc53140bcf97eae0a50
Yosendra <yosendra.evri@gmail.com>
2024-03-23 17:40:32 +0800

    Introduction - 01. Project Initial

---
+++ 01. Introduction/01. Project Initial/GigHub/Global.asax.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* When generating solution, choose template for ASP.NET Web Applicaiton (.NET Framework)
+ * Choose the MVC template, don't forget to choose the authentication to Individual Account
+ * This will include Identity package for our login feature
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
