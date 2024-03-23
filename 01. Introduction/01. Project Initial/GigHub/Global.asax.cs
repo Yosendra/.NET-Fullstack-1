1104555f643edcfa311d5fd03ceda6c5a8cb1e7e
Yosendra <yosendra.evri@gmail.com>
2024-03-23 17:51:56 +0800

    Add notes

--- 01. Introduction/01. Project Initial/GigHub/Global.asax.cs
+++ 01. Introduction/01. Project Initial/GigHub/Global.asax.cs
@@ -9,6 +9,11 @@ using System.Web.Routing;
 /* When generating solution, choose template for ASP.NET Web Applicaiton (.NET Framework)
  * Choose the MVC template, don't forget to choose the authentication to Individual Account
  * This will include Identity package for our login feature
+ * 
+ * This is optional, you can install these extensions in your Visual Studio to improve your development experience:
+ * (in Extensions ribbon -> Manage Extensions...)
+ * • Productivity Power Tools
+ * • The Essentials
 */
 
 namespace GigHub
