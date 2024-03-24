79563e47f51ea2989722707e4e33685825b57060
Yosendra <yosendra.evri@gmail.com>
2024-03-24 08:57:31 +0800

    Building a Form with Bootstrap - 05. Adding a Bootstrap Button

---
+++ 03. Building a Form with Bootstrap/05. Adding a Bootstrap Button/GigHub/Global.asax.cs
@@ -0,0 +1,23 @@
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* Adding the submit button for the form in Create view
+ * 
+ * Look at:
+ * • Create.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
