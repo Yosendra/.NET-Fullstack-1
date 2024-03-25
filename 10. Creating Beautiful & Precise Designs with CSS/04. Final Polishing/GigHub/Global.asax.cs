210dd465a3eade8d0ed58ae329facd2e4c19170b
Yosendra <yosendra.evri@gmail.com>
2024-03-25 22:13:01 +0800

    Creating Beautiful & Precise Designs with CSS - 04. Final Polishing

---
+++ 10. Creating Beautiful & Precise Designs with CSS/04. Final Polishing/GigHub/Global.asax.cs
@@ -0,0 +1,31 @@
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * We apply some additional css for styling
+ * Artist name and genre text in gigs list
+ * 
+ * Test: Visit home page, now we have calendar-like icon
+ *       for the gig list
+ * 
+ * Look at:
+ * • Site.css
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
