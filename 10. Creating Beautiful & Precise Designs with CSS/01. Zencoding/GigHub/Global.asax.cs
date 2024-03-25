6ba2a8e13fd7dab321557908271ce96da97c9151
Yosendra <yosendra.evri@gmail.com>
2024-03-25 21:32:53 +0800

    Creating Beautiful & Precise Designs with CSS - 01. Zencoding

---
+++ 10. Creating Beautiful & Precise Designs with CSS/01. Zencoding/GigHub/Global.asax.cs
@@ -0,0 +1,30 @@
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * Create the markup for displaying gigs in the HomeController.Index() view
+ * Add eager loading for Genre table in HomeController.Index()
+ * 
+ * Test: run the application, The upcoming gig list display is still not styled
+ * 
+ * Look at:
+ * • HomeController.cs -> Index.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
