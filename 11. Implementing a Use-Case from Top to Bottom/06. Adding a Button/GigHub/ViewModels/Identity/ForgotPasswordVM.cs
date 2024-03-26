5f9eabafd9aa0f77f710c919cf90540dd4b4734d
Yosendra <yosendra.evri@gmail.com>
2024-03-26 15:29:18 +0800

    Implementing a Use-Case from Top to Bottom - 06. Adding a Button

---
+++ 11. Implementing a Use-Case from Top to Bottom/06. Adding a Button/GigHub/ViewModels/Identity/ForgotPasswordVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModels
+{
+    public class ForgotPasswordVM
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
