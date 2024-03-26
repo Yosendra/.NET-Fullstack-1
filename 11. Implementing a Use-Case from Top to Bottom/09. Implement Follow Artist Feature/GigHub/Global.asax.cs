ab152f127544a3f1cb846a8b0875fd9413e6995f
Yosendra <yosendra.evri@gmail.com>
2024-03-26 18:07:20 +0800

    Implementing a Use-Case from Top to Bottom - 09. Implement Follow Artist Feature

---
+++ 11. Implementing a Use-Case from Top to Bottom/09. Implement Follow Artist Feature/GigHub/Global.asax.cs
@@ -0,0 +1,45 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * Implement following artist feature
+ * Check everything under "following artist feature" comment in the file mentioned below
+ * 
+ * After modify "Following.cs" and "ApplicationUser.cs"
+ *  Add-Migration AddFollowing
+ *  Update-Database
+ *  
+ * After that, we create the API for Following named "FollowingsController"
+ * Put the logic for following in FollowingsController.Follow()
+ * 
+ * Now in the Index.cshtml of HomeController
+ * We put some class in html element and javascript code to sending api request
+ * to FollowiingControllers through ajax
+ * 
+ * Look at:
+ * • Following.cs
+ * • ApplicationUser.cs
+ * • 202403260912153_AddFollowing.cs
+ * • FollowingsController.cs -> Follow()
+ * • HomeController.cs -> Index.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
