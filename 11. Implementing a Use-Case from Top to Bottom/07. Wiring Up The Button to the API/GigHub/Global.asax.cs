3ed9cf4a660f195255424e9d04bd335c4ec91606
Yosendra <yosendra.evri@gmail.com>
2024-03-26 15:54:38 +0800

    Implementing a Use-Case from Top to Bottom - 07. Wiring Up The Button to the API

---
+++ 11. Implementing a Use-Case from Top to Bottom/07. Wiring Up The Button to the API/GigHub/Global.asax.cs
@@ -0,0 +1,31 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * We put javascript logic at the bottom of the Index view
+ * Also we add certain attribute in the going button
+ * Look at the Index.cshtml
+ * 
+ * Look at:
+ * • HomeController.cs -> Index.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
